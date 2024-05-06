34ec7339fe0412707a00486646a2a5e8b5982bab
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-05-06 16:02:11 +0700

    update file cardlist

---
+++ src/component/organisms/card-chrom-dowload/CardList.tsx
@@ -0,0 +1,35 @@
+import { dataType } from "../../../pages/ChromeDownload";
+import Card from "./Card";
+
+export interface CardListProps {
+  props: {
+    data: dataType[];
+    setIsMatch: (isMatch: boolean)=> void,
+    setCatchId: (catchId: number)=> void
+    setIsUpdate: (isUpdate: boolean) => void
+  };
+}
+
+function CardList({ props }: CardListProps) {
+ const data = props.data;
+ const handleSetIsMatch = props.setIsMatch;
+ const handleSetCatchId = props.setCatchId;
+ const handleSetIsUpdate = props.setIsUpdate
+  return (
+    <div key={""} className=" grid grid-cols-1 gap-5">
+      {data.map((item, key) => (
+        <Card
+          key={key}
+          props={{
+            ...item,
+            setIsMatch: handleSetIsMatch,
+            setCatchId: handleSetCatchId,
+            setIsUpdate: handleSetIsUpdate
+          }}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default CardList;
