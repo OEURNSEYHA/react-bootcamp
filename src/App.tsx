add67b9a6d3ef0eabbf525a01eb9c00ca8a3acfb
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-05-06 15:57:58 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Header from "./component/templates/Header";
 import ChromeDownload from "./pages/ChromeDownload";
 
 function App() {
   return (
     <>
-      {/* component header */}
-      <Header />
+     
       {/* wrap page chrome download */}
       <ChromeDownload />
     </>
