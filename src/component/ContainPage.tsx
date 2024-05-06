ffb1565788008cc68697eb27343d4955d3bef2af
OEURN SEYHA <seyhaoeurn920@gmail.com>
2024-05-06 15:58:30 +0700

    update

--- src/component/ContainPage.tsx
+++ src/component/ContainPage.tsx
@@ -1,57 +1,57 @@
-const ContainPage: React.FC = () => {
+// const ContainPage: React.FC = () => {
  
 
 
 
 
-  return (
-    <>
-      <header>
+//   return (
+//     <>
+//       <header>
   
-        <div className="content-header">
-          <div className="logo">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/2048px-Google_Chrome_icon_%28February_2022%29.svg.png"
-              alt=""
-              width="30"
-            />
-            <span>Downloads</span>
-          </div>
-          <div className="search-bar">
-            <i className="bi bi-search"></i>
-            <input type="text" name="" id="" value={searchTerm} placeholder="Search downloads" onChange={handleSearchChange} />
-          </div>
-          <div className="setting"><i className="bi bi-three-dots-vertical"></i></div>
-        </div>
-      </header>
-      <section className="wrap-page">
-        <div style={{ "display": "flex" }}>
-          <div style={{ "width": "20px", "height": "20px", "marginLeft": "10px" }}>
-            <i className="bi bi-buildings"></i>
-          </div>
-          Your <a href=""> Profile is managed </a>by sabaicode.com
-        </div>
+//         <div className="content-header">
+//           <div className="logo">
+//             <img
+//               src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Google_Chrome_icon_%28February_2022%29.svg/2048px-Google_Chrome_icon_%28February_2022%29.svg.png"
+//               alt=""
+//               width="30"
+//             />
+//             <span>Downloads</span>
+//           </div>
+//           <div className="search-bar">
+//             <i className="bi bi-search"></i>
+//             <input type="text" name="" id="" value={searchTerm} placeholder="Search downloads" onChange={handleSearchChange} />
+//           </div>
+//           <div className="setting"><i className="bi bi-three-dots-vertical"></i></div>
+//         </div>
+//       </header>
+//       <section className="wrap-page">
+//         <div style={{ "display": "flex" }}>
+//           <div style={{ "width": "20px", "height": "20px", "marginLeft": "10px" }}>
+//             <i className="bi bi-buildings"></i>
+//           </div>
+//           Your <a href=""> Profile is managed </a>by sabaicode.com
+//         </div>
 
-        <div className="content-item">
-          {Object.keys(groupedHistory).map(date => (
-            <div key={date} className="box-item">
-              <h3> {date}</h3>
-              {groupedHistory[date].map(item => (
-                <div key={item.id} className="item">
-                  <div className="img">image</div>
-                  <div className="desc">
-                    <a href="">{item.name}</a>
-                    <span>...https://us-easet-1.console.aws.amazon.com/ jej9300339598958-kljksjfdlk034kmlajd0343034004</span>
-                    <i className="bi bi-three-dots-vertical"></i>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </section>
-    </>
-  );
-}
+//         <div className="content-item">
+//           {Object.keys(groupedHistory).map(date => (
+//             <div key={date} className="box-item">
+//               <h3> {date}</h3>
+//               {groupedHistory[date].map(item => (
+//                 <div key={item.id} className="item">
+//                   <div className="img">image</div>
+//                   <div className="desc">
+//                     <a href="">{item.name}</a>
+//                     <span>...https://us-easet-1.console.aws.amazon.com/ jej9300339598958-kljksjfdlk034kmlajd0343034004</span>
+//                     <i className="bi bi-three-dots-vertical"></i>
+//                   </div>
+//                 </div>
+//               ))}
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
 
-export default ContainPage;
+// export default ContainPage;
